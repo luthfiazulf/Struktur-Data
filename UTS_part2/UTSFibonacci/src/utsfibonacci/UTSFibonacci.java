20372635e6c1e44f2d736c21957ac1c3b798c199
luthfiazulf <lutfiadila19@gmail.com>
2026-04-23 11:17:10 +0700

    Add files via upload

---
+++ UTS_part2/UTSFibonacci/src/utsfibonacci/UTSFibonacci.java
@@ -0,0 +1,33 @@
+package utsfibonacci;
+
+import java.util.Scanner;
+
+public class UTSFibonacci {
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Masukkan jumlah suku Fibonacci: ");
+        
+        int n= input.nextInt();
+        System.out.println("Deret Fibonacci hingga suku ke-" + n + " adalah:");
+        
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fibonacci(i));
+            if (i < n) {
+                System.out.print(", ");
+        }    
+    }
+        System.out.println();
+    }
+    public static int fibonacci (int n) {
+        if (n==0) {
+            return 0;
+        }
+        else if (n==1) {
+            return 1;
+        }
+        else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+}
